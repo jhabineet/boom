c0c45e4b7ebfd8af756a7e4b88f378000e9e5952
Bineet Jha <jhabineet0@gmail.com>
2025-08-04 11:57:13 +0530

    added some more functionality with some code

--- main.js
+++ main.js
@@ -2,3 +2,7 @@ console.log(heelo);
 console.log("Hello, World!");
 console.log("This is a test script.");
 console.log("Make sure to check for typos.");
+console.log("This line has a typo: heelo should be hello.");
+console.log("Corrected the typo: heelo to hello.");
+console.log("End of script.");
+console.log("All done!");
